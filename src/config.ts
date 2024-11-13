28879e6682813a6a59e1f8c44a75c26708235a93
gezhe <htt3001ptq@hotmail.com>
2024-11-13 13:54:10 +1300

    update bio again

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Xingyi',
-  bio: 'Sharing insights, experiences from a journey in front-end and full-stack development, along with more topics.',
+  bio: 'Insights and experiences about front-end, full-stack development, and more.',
   links: [
     {
       name: 'Twitter',
